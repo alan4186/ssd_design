8189d043ff637d23e9f176cfe818927edaa6a1ca
Alan Ehret <alan.4186@yahoo.com>
2015-09-28 21:36:16 -0400

    changed translator to use megafunction for ddr_dq(s)

--- flash_interface/command_translator.v
+++ flash_interface/command_translator.v
@@ -1,30 +1,27 @@
-﻿module command_translator (
-	// flash ports
-	dq,
-	dqs,
-	clk,
-	wr,
-	ale,
-	cle,
-	ce1,
-	ce2,
-	wp,
-	rb1,
-	rb2,
+module command_translator (// flash ports
+	output clk,
+	output reg wr,
+	output reg ale,
+	output reg cle,
+	output reg ce1,
+	output reg ce2,
+	output reg wp,
+	input rb1,
+	input rb2,
 	// controler ports
-	clock_100,
-	rst,
-	data_ctrl2trans, // data that is input from controler
-	data_trans2ctrl,// tata that is read from flash
-	dqs_ctrl2trans,
-	dqs_trans2ctrl,
-	rb1_ctrl,
-	rb2_ctrl,
-	ce
+	input clock_100,
+	input rst,
+	input [0:cmd_width_short] cmd,
+	output rb1_ctrl,
+	output rb2_ctrl,
+	input ce,
+	// ports to ddr megafunction
+	output reg oe
 	);
 	parameter cmd_width_short = 7,
 				 cmd_width_full = cmd_width_short +1;
-				 
+	
+	
 	// parameters to define modes
 	// i/o names are realative to flash chip
 	parameter standby = 3'd0,
@@ -36,122 +33,102 @@
 				 data_output = 3'd6,
 				 write_protect = 3'd7;
 	
-	// inputs from control
-	input clock_100, rst, ce, dqs_ctrl2trans;
-	// input/outputs from/to control
-	inout [0:cmd_width_short] cmd;
-	// outputs to control
-	output dqs_trans2ctrl
-	// inputs from flash
-	input rb1, rb2;
-	// input outputs from/ to flash
-	inout dqs;
-	inout [0:7] = dq;
-	// outputs to flash
-	output wr, ale, cle, ce1,ce2, wp;
 	
-	// regs
+
 	
 	assign clk = clock_100;// pass clock to flash	
 	assign rb1_ctrl = rb1;
 	assign rb2_ctrl = rb2;
 
-	// assigns for inouts
-			// will either be feedback or data from flash
-	assign data_trans2ctrl = dq;
-	always@(*)
-	begin
-		case(data_ctrl2trans)
-			begin 
-			case command_input:
-				dq <= data_ctrl2trans;
-			case address_input:
-				dq <= data_ctrl2trans;
-			case data_input:
-				dq <= data_ctrl2trans;
-			default
-			   dq <= h'zz;
-			endcase
-	end
-	
-	assign dqs_trans2ctrl = dqs;
-        assign dqs = (state == bus_drive || state == data_output) ? 1'bz : dqs_ctrl2trans;
-
 	// posedge block for control signals
 	always@(posedge clock_100 or negedge rst)
 	begin
-		if(rst == 1'b0) begin
-		
-	end 
-	else
-	begin
-		case (cmd)
-		begin 
-		case standby:
+		if(rst == 1'b0) 
 		begin
+		   oe <= 1'b0;
 			wr <= 1'bx;
 			ale <= 1'bx;
 			cle <= 1'bx;
 			ce1 <= 1'b1;
 			ce2 <= 1'b1;
-			wp <= 1'b
-		end
-		case bus_idle:
+			wp <= 1'b1;// needs to be cmos high
+		end 
+	else
+	begin
+		case (cmd) 
+		standby:
 		begin
+			oe <= 1'b0;
+			wr <= 1'bx;
+			ale <= 1'bx;
+			cle <= 1'bx;
+			ce1 <= 1'b1;
+			ce2 <= 1'b1;
+			wp <= 1'b1;// needs to be cmos high
+		end
+		bus_idle:
+		begin
+			oe <= 1'b0;
 			wr <= 1'b1; 
 			ale <= 1'b0;
 			cle <= 1'b0;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'bx;
 		end
-		case bus_driving:
+		bus_driving:
 		begin
+			oe <= 1'b0;
 			wr <= 1'b0;
 			ale <= 1'b0;
 			cle <= 1'b0;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'bx;
 		end
-		case command_input:
+		command_input:
 		begin
+			oe <= 1'b1;
 			wr <= 1'b1;
 			ale <= 1'b0;
 			cle <= 1'b1;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'b1;
 		end
-		case address_input:
+		address_input:
 		begin
+			oe <= 1'b1;
 			wr <= 1'b1;
 			ale <= 1'b1;
 			cle <= 1'b0;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'b1;
 		end
-		case data_input:
+		data_input:
 		begin
+			oe <= 1'b1;
 			wr <= 1'b1;
 			ale <= 1'b1;
 			cle <= 1'b1;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'b1;
 		end
-		case data_output:
+		data_output:
 		begin
+			oe <= 1'b0;
 			wr <= 1'b0;
 			ale <= 1'b1;
 			cle <= 1'b1;
-			ce1 <= ce | 1'b1;
-			ce2 <= !ce | 1'b1;
+			ce1 <= ce & 1'b1;
+			ce2 <= !ce & 1'b1;
 			wp <= 1'bx;
 		end
-		case write_protect:
+		write_protect:
 		begin
+			oe <= 1'b0;
 			wr <= 1'bx;
 			ale <= 1'bx;
 			cle <= 1'bx;
@@ -161,65 +138,7 @@
 		end
 		endcase
 	end
+	end
 	
-	// posedge and negedge block for ddr i/o
-	// for now, assume that the same clk drives
-	// flash and control logic.
-	
-	// dq and dqs are defined as 
-	// inputs for dont care states
-//	always@(posedge clock_100 or negedge rst, clock_100)
-//	begin
-//		if (rst == 1'b0)
-//		begin
-//		
-//		end
-//		else
-//		begin
-//			case (cmd)
-//			begin 
-//			case standby:
-//			begin
-//				dq
-//				dqs
-//			end
-//			case bus_idle:
-//			begin
-//				dq
-//				dqs
-//			end
-//			case bus_driving:
-//			begin
-//				dq;
-//				dqs		
-//			end
-//			case command_input:
-//			begin
-//				dq
-//				dqs	
-//			end
-//			case address_input:
-//			begin
-//				dq
-//				dqs
-//			end
-//			case data_input:
-//			begin
-//				dq 
-//				dqs
-//			end
-//			case data_output:
-//			begin
-//				dq;
-//				dqs 	
-//			end
-//			case write_protect:
-//			begin
-//				dq
-//				dqs
-//			end
-//			endcase
-//		end
-//	end
-//	
+
 	endmodule
---
+++ flash_interface/level_one.v
@@ -0,0 +1,64 @@
+module level_one (
+	output clk,
+	output wr,
+	output ale,
+	output cle,
+	output ce1,
+	output ce2,
+	output wp,
+	input rb1,
+	input rb2,
+	// controler ports
+	input clock_100,
+	input rst,
+	input wire [0:cmd_width_short] cmd,
+	output rb1_ctrl,
+	output rb2_ctrl,
+	input ce,
+	// ddr_dq ports
+	input   [8:0]  datain_h,
+	input   [8:0]  datain_l,
+	output   [8:0]  dataout_h,
+	output   [8:0]  dataout_l,
+	inout   [8:0]  padio
+);
+
+parameter cmd_width_short = 7,
+			 cmd_width_full = cmd_width_short +1;
+
+wire oe;
+//wire [cmd_width_short:0] cmd;
+
+command_translator translator(	
+	 clk,
+	 wr,
+	 ale,
+	 cle,
+	 ce1,
+	 ce2,
+	 wp,
+	 rb1,
+	 rb2,
+	// controler ports
+	 clock_100,
+	 rst,
+	 cmd,
+	 rb1_ctrl,
+	 rb2_ctrl,
+	 1'd0,
+	 oe
+);
+
+ddr_dq_dq_b6o ddr_dq_a
+	( 
+	datain_h,
+	datain_l,
+	dataout_h,
+	dataout_l,
+	clock_100,//inclock,
+	oe,
+	clock_100,//outclock,
+	padio);
+	
+endmodule
+	
